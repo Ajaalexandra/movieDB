967b221b4fb751684524dec9cc3a5b52eec9aa79
Ajaalexandra <ajacederberg@yahoo.com>
2018-02-14 17:58:47 -0500

    chore: delete logo

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { json } = require("body-parser");
 const axios = require("axios");
-
+const { apiURL, apiKey } = require("../config");
 //Initialize Server
 const app = express();
 const port = 3001;
@@ -16,6 +16,8 @@ app.use(json());
 app.get("/api/test", (req, res, next) => {
   res.status(200).json("test successful");
 });
+
+console.log(apiURL, apiKey);
 
 app.listen(port, () => {
   console.log(`SERVER IS LISTENING ON PORT: ${port}`);
